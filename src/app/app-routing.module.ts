15eeea34207b0ca2a62bdbd31970c9280710f057
Satyaansh <satyanshasthana@gmail.com>
2023-07-17 18:44:11 +0530

    Add view post to routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,9 +6,11 @@ import { HomeComponent } from './home/home.component';
 import { CreatePostComponent } from './post/create-post/create-post.component';
 import { CreateGameComponent } from './game/create-game/create-game.component';
 import { ListGamesComponent } from './game/list-games/list-games.component';
+import { ViewPostComponent } from './post/view-post/view-post.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
+  { path: 'view-post/:id', component: ViewPostComponent },
   { path: 'sign-up', component: SignUpComponent },
   { path: 'login', component: LoginComponent },
   { path: 'create-post', component: CreatePostComponent},
