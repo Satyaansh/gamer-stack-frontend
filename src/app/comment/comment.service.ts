0ea2d230b5037eec2673cfb600005f24ee3e866c
Satyaansh <satyanshasthana@gmail.com>
2023-07-28 17:50:27 +0530

    Implement comment service

--- src/app/comment/comment.service.ts
+++ src/app/comment/comment.service.ts
@@ -1,9 +1,21 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { CommentPayload } from './comment-payload';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CommentService {
 
-  constructor() { }
+  constructor(private httpClient: HttpClient) { }
+
+  getAllCommentsForPost(postId: number): Observable<CommentPayload[]> {
+    let params = new HttpParams().set("postId", postId);
+    return this.httpClient.get<CommentPayload[]>('http://localhost:8080/api/comments/', {params});
+  }
+
+  postComment(commentPayload: CommentPayload): Observable<any> {
+    return this.httpClient.post<any>('http://localhost:8080/api/comments/', commentPayload);
+  }
 }
